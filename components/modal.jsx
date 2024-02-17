da26072ade6bc109f7bc80d099393552a83eff6d
Abdul Rehman <abdulrehman@nikahforever.com>
2024-02-17 14:22:08 +0530

    update bg color modal

--- components/modal.jsx
+++ components/modal.jsx
@@ -1,4 +1,3 @@
-// Modal.js
 import { useState } from 'react';
 import "@styles/style.css";
 import MyForm from '@/components/form';
@@ -9,19 +8,26 @@ const Modal = ({ showModal, onClose }) => {
   };
 
   return (
-    <div className={`z-index-9999 modal ${showModal ? 'show' : ''}`} tabIndex="-2" role="dialog" style={{ display: showModal ? 'block' : 'none' }}>
-      <div className="modal-dialog modal-dialog-centered">
-        <div className="modal-content rounded-3 shadow-lg">
-          <div className="modal-header bg-light">
-            <h5 className="modal-title text-dark">Register for Free Consultancy</h5>
-            <button type="button" className="btn-close" onClick={closeModal}></button>
-          </div>
-          <div className="modal-body text-bg-light">
-            <MyForm closeModal={closeModal} />
+    <>
+      {showModal && (
+        <div>
+          <div className="modal-backdrop" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}></div>
+          <div className={`z-index-9999 modal show`} tabIndex="-2" role="dialog" style={{ display: 'block' }}>
+            <div className="modal-dialog modal-dialog-centered">
+              <div className="modal-content rounded-3 shadow-lg">
+                <div className="modal-header bg-light">
+                  <h5 className="modal-title text-dark">Register for Free Consultancy</h5>
+                  <button type="button" className="btn-close" onClick={closeModal}></button>
+                </div>
+                <div className="modal-body text-bg-light">
+                  <MyForm closeModal={closeModal} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
