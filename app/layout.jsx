431da3d04d66144636cbc639eedb9b8ec9a0a857
Abdul Rehman <abdulrehman@nikahforever.com>
2024-03-03 16:15:38 +0530

    meta pixel

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,19 +24,32 @@ const RootLayout = ({ children }) => (
         `}
         </Script>
 
+              {/* Meta Pixel Code */}
         <script
-          type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-              window.smartlook||(function(d) {
-                var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-                var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-                c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-              })(document);
-              smartlook('init', 'cb0cb103185e7ecce86aec5b0bf9f55f8dc82df8', { region: 'eu' });
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1067039781034205');
+              fbq('track', 'PageView');
             `,
           }}
         />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1067039781034205&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         
     </Head>
     <body>
