253e22fbb3504dbb1d757202b5551985677f3f13
Abdul Rehman <abdulrehman@nikahforever.com>
2024-03-04 22:36:05 +0530

    psuh meta n oogle

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import "@styles/style.css";
-import Script from "next/script";
-import Head from 'next/head';
+import Scripts from "@/components/scripts";
 
 export const metadata = {
   title: "Vidyarthiadda | Find the best college in the city",
@@ -9,41 +8,9 @@ export const metadata = {
 
 const RootLayout = ({ children }) => (
   <html lang='en'>
-    <Head>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=G-93KTPMMH3S`}
-        strategy="afterInteractive"
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-93KTPMMH3S');
-        `}
-      </Script>
-
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1067039781034205');
-    fbq('track', 'PageView');
-    `,
-        }}
-      />
-
-    </Head>
+    <head>
+      <Scripts/>
+    </head>
     <body>
       <div className='main'>
         <div className='gradient' />
