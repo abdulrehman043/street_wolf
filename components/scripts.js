0daa6efa6bfb6aa38165be001097ba37206ca31c
Abdul Rehman <abdulrehman@nikahforever.com>
2024-03-04 22:39:16 +0530

    update meta pixel

--- components/scripts.js
+++ components/scripts.js
@@ -9,23 +9,18 @@ const Scripts = () => {
   `;
 
   const facebookPixelScript = `
-    !function (f, b, e, v, n, t, s) {
-      if (f.fbq) return;
-      n = f.fbq = function () { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments); };
-      if (!f._fbq) f._fbq = n;
-      n.push = n;
-      n.loaded = !0;
-      n.version = '2.0';
-      n.queue = [];
-      t = b.createElement(e);
-      t.async = !0;
-      t.src = v;
-      s = b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t, s);
-    }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1067039781034205');
-    fbq('track', 'PageView');
-  `;
+  <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1067039781034205');
+  fbq('track', 'PageView');
+  </script>`;
 
   return (
     <>
@@ -35,6 +30,9 @@ const Scripts = () => {
 
       {/* Facebook Pixel */}
       <script dangerouslySetInnerHTML={{ __html: facebookPixelScript }} />
+      <noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1067039781034205&ev=PageView&noscript=1"
+  /></noscript>
     </>
   );
 };
