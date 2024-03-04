4491767bc48b94466b8cd2840b365257a556e8eb
Abdul Rehman <abdulrehman@nikahforever.com>
2024-03-04 22:45:05 +0530

    deployement error fix

--- components/scripts.js
+++ components/scripts.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Scripts = () => {
-  const googleAnalyticsScript = `
+    const googleAnalyticsScript = `
     window.dataLayer = window.dataLayer || [];
     function gtag() { window.dataLayer.push(arguments); }
     gtag('js', new Date());
     gtag('config', 'G-93KTPMMH3S');
   `;
 
-  const facebookPixelScript = `!function(f,b,e,v,n,t,s)
+    const facebookPixelScript = `!function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -19,19 +19,25 @@ const Scripts = () => {
   fbq('init', '1067039781034205');
   fbq('track', 'PageView');`;
 
-  return (
-    <>
-      {/* Google Analytics */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-93KTPMMH3S"></script>
-      <script dangerouslySetInnerHTML={{ __html: googleAnalyticsScript }} />
+    return (
+        <>
+            {/* Google Analytics */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-93KTPMMH3S"></script>
+            <script dangerouslySetInnerHTML={{ __html: googleAnalyticsScript }} />
 
-      {/* Facebook Pixel */}
-      <script dangerouslySetInnerHTML={{ __html: facebookPixelScript }} />
-      <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=1067039781034205&ev=PageView&noscript=1"
-  /></noscript>
-    </>
-  );
+            {/* Facebook Pixel */}
+            <script dangerouslySetInnerHTML={{ __html: facebookPixelScript }} />
+            <noscript>
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: 'none' }}
+                    src="https://www.facebook.com/tr?id=1067039781034205&ev=PageView&noscript=1"
+                />
+            </noscript>
+
+        </>
+    );
 };
 
 export default Scripts;
